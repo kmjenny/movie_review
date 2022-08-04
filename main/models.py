59f4280839a3becfe9ab6c8b15a89c299b20e3b5
kmjenny <kjn3008@gmail.com>
2022-08-04 23:53:49 +0900

    과제 제출

---
+++ main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    content = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    user = models.CharField(max_length=12)
+    name = models.CharField(max_length=50)
+    comment = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ main/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'name', 'content', 'created_at', 'updated_at')
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
